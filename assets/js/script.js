558b3e02419180f62a5b7be8b574c14007bb097a
Jennifer Byrnes <jennifer.byrnes@outlook.com>
2022-07-05 19:41:13 -0400

    restart

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+//  This is where all global variable should be declared.
+
+//*******************************************************/
+//             Savannah's code goes here                */
+var savannahFunction = function(){
+  console.log("Savannah's Function call works");
+}
+
+//*******************************************************/
+//             Jennifer's code goes here                */
+var jenniferFunction = function(){
+  console.log("Jennifer's Function call works");
+}
+
+//*******************************************************/
+//             Amanda's code goes here                */
+var amandaFunction = function(){
+  console.log("Amanda's Function call works");
+}
+
+savannahFunction();
